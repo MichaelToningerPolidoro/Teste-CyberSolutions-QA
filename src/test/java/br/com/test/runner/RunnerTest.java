d26d02c357127b54c994911f21deb48a943cf22b
Michael Toninger <michaeltp.2012@hotmail.com>
2024-02-06 19:57:28 -0300

    adicionado dependências no projeto

---
+++ src/test/java/br/com/test/runner/RunnerTest.java
@@ -0,0 +1,9 @@
+package br.com.test.runner;
+
+public class RunnerTest {
+
+    // TODO: Implement runner here
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaa");
+    }
+}
