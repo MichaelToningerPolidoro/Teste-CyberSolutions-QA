975c5d54320d58a8fccda97c2b11603208317915
Michael Toninger <michaeltp.2012@hotmail.com>
2024-02-08 20:07:17 -0300

    Added EnvVarsProvider

---
+++ src/test/java/br/com/test/core/settings/EnvVarsProvider.java
@@ -0,0 +1,36 @@
+package br.com.test.core.settings;
+
+import java.util.NoSuchElementException;
+
+/**
+ * TODO: Javadocs
+ * */
+public final class EnvVarsProvider {
+
+    /**
+     * TODO: Javadocs
+     * */
+    public static String getBrowser() {
+        return getEnv("browser");
+    }
+
+    /**
+     * TODO: Javadocs
+     * */
+    public static String getHeadless() {
+        return getEnv("headless");
+    }
+
+    /**
+     * TODO: Javadocs
+     * */
+    private static String getEnv(String var) {
+        final String value = System.getenv(var);
+
+        if (value == null) {
+            throw new NoSuchElementException("Unable to find the environment variable -> " + var);
+        }
+
+        return value;
+    }
+}
