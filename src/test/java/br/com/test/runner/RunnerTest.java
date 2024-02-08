cf070b0d7b61b9ce6f20c7c2fc982170781f04c7
Michael Toninger <michaeltp.2012@hotmail.com>
2024-02-08 19:40:16 -0300

    Ajuste no mapeamento do glue do runner

--- src/test/java/br/com/test/runner/RunnerTest.java
+++ src/test/java/br/com/test/runner/RunnerTest.java
@@ -7,7 +7,7 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "br/com/test/steps")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "br/com/test")
 @ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME, value = "src/test/resources/features")
 @ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME,value = "@tempTest")
 @ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME,value = "false")
