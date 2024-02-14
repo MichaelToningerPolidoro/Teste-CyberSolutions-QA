954cf9475e783e87c3e8c650279c570be9e70994
Michael Toninger <michaeltp.2012@hotmail.com>
2024-02-14 19:31:01 -0300

    Removed WebDriverProvider

--- src/test/java/br/com/test/core/driver/DriverManager.java
+++ src/test/java/br/com/test/core/driver/DriverManager.java
@@ -7,21 +7,36 @@ import org.openqa.selenium.WebDriver;
  * */
 public final class DriverManager {
 
+    private static final ThreadLocal<WebDriver> webDriverPool = new ThreadLocal<>();
+
     /**
      * TODO: Javadocs
      * */
     public static void initializeWebDriver() {
-        DriverProvider.getWebDriver();
+        getWebDriver();
+    }
+
+    /**
+     * TODO: Javadocs
+     * */
+    public static WebDriver getWebDriver() {
+        if (webDriverPool.get() == null) {
+            webDriverPool.set(DriverFactory.createWebDriver());
+        }
+
+        return webDriverPool.get();
     }
 
     /**
      * TODO: Javadocs
      * */
     public static void killWebDriver() {
-        final WebDriver webDriver = DriverProvider.getWebDriver();
+        final WebDriver webDriver = getWebDriver();
 
         if (webDriver != null) {
             webDriver.quit();
         }
+
+        webDriverPool.remove();
     }
 }
