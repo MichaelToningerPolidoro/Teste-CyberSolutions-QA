c832f8c1f7bb99faa690d2ab83cea3eca09e6026
Michael Toninger <michaeltp.2012@hotmail.com>
2024-02-14 19:30:47 -0300

    Added class to handle DriverArguments

---
+++ src/test/java/br/com/test/core/driver/DriverArguments.java
@@ -0,0 +1,27 @@
+package br.com.test.core.driver;
+
+import org.openqa.selenium.PageLoadStrategy;
+
+/**
+ * TODO: Javadocs
+ * */
+public final class DriverArguments {
+
+    private DriverArguments() {}
+
+    public static final PageLoadStrategy STANDARD_PAGE_LOAD_STRATEGY = PageLoadStrategy.NORMAL;
+
+    public static final String[] STANDARD_CHROMIUM_ARGUMENTS = {
+            "--start-maximized",
+            "--disable-infobars",
+            "--disable-translate",
+            "--disable-notifications",
+            "--disable-extensions"
+    };
+
+    public static final String[] STANDARD_CHROMIUM_HEADLESS_ARGUMENTS = {
+            "--headless",
+            "--disable-gpu",
+            "window-size=1920,1080"
+    };
+}
